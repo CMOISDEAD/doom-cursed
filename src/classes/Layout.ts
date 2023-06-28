0cf998ce6687fd7ce90d985cf5bf50c778970d62
CMOISDEAD <camilodavina@gmail.com>
2023-06-27 22:36:35 -0500

    feat: clean the screen before show the sections

--- src/classes/Layout.ts
+++ src/classes/Layout.ts
@@ -1,19 +1,24 @@
 import { Section } from "../index.d";
 import { print_content } from "../utils";
 
-export class Layout implements Layout {
+export class Layout {
   sections: Section[];
 
   constructor(sections: Section[]) {
     this.sections = sections;
   }
 
+  pre_render(): void {
+    console.clear();
+  }
+
   render(): void {
+    this.pre_render();
     const totalWidth = process.stdout.columns || 80;
     // const totalHeight = process.stdout.rows || 24;
     let lastWidth = 0;
 
-    this.sections.forEach((section, i) => {
+    this.sections.forEach((section) => {
       const width = Math.round((section.width * totalWidth) / 100);
       process.stdout.cursorTo(lastWidth);
       process.stdout.write(print_content(section.content, width));

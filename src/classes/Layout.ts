24bd338fe8a1d4ceef4be04d73b692dec28dc85d
CMOISDEAD <camilodavina@gmail.com>
2024-06-15 11:43:47 -0500

    xd

--- src/classes/Layout.ts
+++ src/classes/Layout.ts
@@ -15,13 +15,14 @@ export class Layout {
   render(): void {
     this.pre_render();
     const totalWidth = process.stdout.columns || 80;
-    // const totalHeight = process.stdout.rows || 24;
+    const totalHeight = process.stdout.rows || 24;
     let lastWidth = 0;
 
     this.sections.forEach((section) => {
       const width = Math.round((section.width * totalWidth) / 100);
+      const height = Math.round((section.height * totalHeight) / 100);
       process.stdout.cursorTo(lastWidth);
-      process.stdout.write(print_content(section.content, width));
+      process.stdout.write(print_content(section.content, width, height));
       lastWidth += width;
     });
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,13 @@
-export const print_content = (content: string, times: number): string => {
-  times -= content.length;
-  return `${content}${" ".repeat(times + 3)}`;
+export const print_content = (
+  content: string,
+  width: number,
+  height: number
+): string => {
+  width -= content.length;
+  let section = "";
+  for (let i = 0; i < height; i++) {
+    section += content.slice(i * width, (i + 1) * width);
+    // if (i !== height - 1) section += "\n";
+  }
+  return section;
 };
